79a0cce2c01de04f098a12e93b64e4aaff0a614b
tomdecavele <32090217+tomdecavele@users.noreply.github.com>
2017-10-18 09:09:13 +0200

    Update

--- Windows Server/Mijn School/3.Making ADUserAccounts.ps1
+++ Windows Server/Mijn School/3.Making ADUserAccounts.ps1
@@ -1,4 +1,7 @@
 ﻿$UserNames = Import-Csv ".\UserAccounts.csv" -Delimiter ";"
+
+$HomeServer="\\team10-ms"
+$HomeShare="Homedirs" 
  
 Foreach ($User in $UserNames)
 { 
@@ -8,11 +11,17 @@ Foreach ($User in $UserNames)
 	$GivenName = $User.GivenName
 	$SurName = $User.SurName
 	$HomeDrive = $User.HomeDrive
-    $HomeDirectory = $User.HomeDirectory
+    $HomeDirectory = $HomeServer+"\"+$HomeShare+"\"+$User.Name
     $ScriptPath = $User.ScriptPath
 	$Path = $User.Path
 
 	$AccountPassword = ConvertTo-SecureString $User.AccountPassword -AsPlainText -force
 
 	New-ADUser -Name $Name -SamAccountName $SamAccountName -DisplayName $DisplayName -GivenName $GivenName -Surname $SurName -HomeDrive $HomeDrive -HomeDirectory $HomeDirectory -ScriptPath $ScriptPath -Path $Path -AccountPassword $AccountPassword -Enabled:$true
+	New-Item -Path $HomeDirectory -type directory -Force
+	$acl = Get-Acl $HomeDirectory
+	$acl.SetAccessRuleProtection($False, $False)
+	$rule = New-Object System.Security.AccessControl.FileSystemAccessRule($User.name,"Modify", "ContainerInherit, ObjectInherit", "None", "Allow")
+	$acl.AddAccessRule($rule)
+	Set-Acl $HomeDirectory $acl
 }
--- Windows Server/Playing around with shares and NTFS permissions on folders.ps1
+++ Windows Server/Playing around with shares and NTFS permissions on folders.ps1
@@ -2,6 +2,12 @@
 # making a share and set NTFS permissions remotely 
 #
 
+#
+# making a share remotely
+# - name : homedirs
+# - share perms : everyone - full control
+# - NTFS perms : Administrators - full control and Authenticated Users - ReadAndExecute on this folder only 
+#
 New-Item -Path \\team10-ms\C$\Homedirs -type directory -Force
 New-SmbShare -CimSession team10-ms –Name Homedirs –Path c:\Homedirs -FullAccess Everyone
 
@@ -16,8 +22,12 @@ $acl.AddAccessRule($rule)
 
 Set-Acl \\team10-ms\c$\Homedirs $acl
 
-
-
+#
+# making a share remotely
+# - name : profiles
+# - share perms : everyone - full control
+# - NTFS perms : Administrators - full control and Authenticated Users - Modify
+#
 New-Item -Path \\team10-ms\C$\Profiles -type directory -Force 
 New-SmbShare -CimSession team10-ms –Name Profiles –Path c:\Profiles -FullAccess Everyone
 
@@ -32,8 +42,12 @@ $acl.AddAccessRule($rule)
 
 Set-Acl \\team10-ms\c$\Profiles $acl
 
-
-
+#
+# making a share remotely
+# - name : public
+# - share perms : everyone - full control
+# - NTFS perms : Administrators - full control and Authenticated Users - ReadAndExecute
+#
 New-Item -Path \\team10-ms\C$\Public -type directory -Force 
 New-SmbShare -CimSession team10-ms –Name Public –Path c:\Public -FullAccess Everyone
 
